b64506ca856701a5ef88e5344e087fe4ae7f15d2
kameo <kameobingham@gmail.com>
2018-03-28 17:14:46 -0400

    added .fadeTo for javascript funtion

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
-$('.image').on('click keypress', function(){
+$('.image').on('click',function(){
 //enlarge image when the element is clicked or when a key is pressed
+$('.image') .fadeTo ("slow", 0.65);
 });
