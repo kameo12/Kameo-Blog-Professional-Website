88e89d9f51fc9634df2517afbdb0acd5dfdf0526
kameo <kameobingham@gmail.com>
2018-03-29 15:24:21 -0400

    Adding additonal comments, fixed spelling error

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
+//Javascript function: changes opacity of headshot image when click on, must refresh page to back to original opacity.
 $('.image').on('click',function(){
-//enlarge image when the element is clicked or when a key is pressed
+//changes image opacity when the headshot image is clicked on
 $('.image') .fadeTo ("slow", 0.65);
 });
